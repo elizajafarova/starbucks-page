da9b7f5b468473be2b01c0f6263322681286dd3d
elizajafarova <eliza.cafarova.f@asoiu.edu.az>
2020-12-07 15:19:46 +0400

    starbucks-page

---
+++ main.js
@@ -0,0 +1,9 @@
+function imgSlider(anything) {
+  //Get the first element in the document with class="example":
+  // document.querySelector(".example");
+    document.querySelector('.starbucks').src = anything;
+  }
+function changeCircleColor(color) {
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
